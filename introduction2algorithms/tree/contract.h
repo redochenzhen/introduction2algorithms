60b04b79f90f06c8a04dd7d19887d5abfd851e5d
Coeus Chen <coeus.chen@hotmail.com>
2014-04-04 21:36:44 +0800

    Some contracts.

---
+++ introduction2algorithms/tree/contract.h
@@ -0,0 +1,17 @@
+/*
+ * contract.h
+ *
+ *  Created on: 2014Äê4ÔÂ1ÈÕ
+ *      Author: Coeus
+ */
+
+#ifndef CONTRACT_H_
+#define CONTRACT_H_
+
+#define NIL ((void*)0)
+#define INT_MAX 0x7FFFFFFF
+#define INT_MIN 0x80000000
+
+typedef enum{false=0,true=1}bool;
+
+#endif /* CONTRACT_H_ */
